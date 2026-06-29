37d59162589bfad20caf5af8eadba50ddd20ce0a
1Voix1 <152695778+1Voix1@users.noreply.github.com>
2026-06-29 22:16:26 +0500

    add config file

---
+++ src/engine/shared/config_variables_rclient.h
@@ -0,0 +1,9 @@
+// This file can be included several times.
+
+#ifndef MACRO_CONFIG_INT
+#error "The config macros must be defined"
+#define MACRO_CONFIG_INT(Tcme, ScriptName, Def, Min, Max, Save, Desc) ;
+#define MACRO_CONFIG_COL(Tcme, ScriptName, Def, Save, Desc) ;
+#define MACRO_CONFIG_STR(Tcme, ScriptName, Len, Def, Save, Desc) ;
+#endif
+
